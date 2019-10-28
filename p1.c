a12c67aae2dfcc11cec70cdd17c068e92881717e
Ebrahim Alhaddad <efalhadd@hpc-login3.hpcc.usc.edu>
2019-10-27 22:58:42 -0700

    2

--- p1.c
+++ p1.c
@@ -4,7 +4,7 @@
 #include <mpi.h>
 int main(int argc, char** argv)
 {
-    int message, size, rank,;
+    int message, size, rank;
     MPI_Init(&argc,&argv);
     MPI_Comm_size(MPI_COMM_WORLD,&size);
     MPI_Comm_rank(MPI_COMM_WORLD,&rank);
