d4c95f4561288826e085c097197217a950f509b1
Ebrahim Alhaddad <efalhadd@hpc-login3.hpcc.usc.edu>
2019-10-27 23:06:47 -0700

    3

---
+++ mpi examples/p1.c
@@ -0,0 +1,41 @@
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
+int main(int argc, char** argv)
+{
+    int message, size, rank;
+    MPI_Init(&argc,&argv);
+    MPI_Comm_size(MPI_COMM_WORLD,&size);
+    MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+    
+    if(rank == 0){
+        message = 450;
+        MPI_Send(&message, 1, MPI_INT, 1, 200, MPI_COMM_WORLD);
+        MPI_Recv(&message, 1, MPI_INT, 3, 200, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+        printf("Process: %d. Message: %d \n", rank, message);
+    }
+    else if(rank == 1){
+        MPI_Recv(&message, 1, MPI_INT, 0, 200, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+        message += 1;
+        MPI_Send(&message, 1, MPI_INT, 2, 200, MPI_COMM_WORLD);
+        printf("Process: %d. Message: %d \n", rank, message);
+    }
+    else if(rank == 2){
+        MPI_Recv(&message, 1, MPI_INT, 1, 200, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+        message += 1;
+        MPI_Send(&message, 1, MPI_INT, 3, 200, MPI_COMM_WORLD);
+        printf("Process: %d. Message: %d \n", rank, message);
+    }
+    else if(rank == 3){
+        MPI_Recv(&message, 1, MPI_INT, 2, 200, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+        message += 1;
+        MPI_Send(&message, 1, MPI_INT, 0, 200, MPI_COMM_WORLD);
+        printf("Process: %d. Message: %d \n", rank, message);
+    }
+    
+    
+    MPI_Finalize();
+    return 0;
+}
+
