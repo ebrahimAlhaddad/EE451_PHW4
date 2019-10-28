9e48e70738b134a1f7045bc0bb90f72c411edfe6
Ebrahim Alhaddad <ebrahimalhaddad@ebrahims-mbp-2.home>
2019-10-28 00:02:14 -0700

    6

--- mpi examples/p2_2.c
+++ mpi examples/p2_2.c
@@ -11,40 +11,34 @@ int main(int argc, char** argv)
     MPI_Comm_size(MPI_COMM_WORLD,&size);
     MPI_Comm_rank(MPI_COMM_WORLD,&rank);
     int *arr = (int*)(malloc(sizeof(int)*64));
-    FILE *fp;
-       char str[MAXCHAR];
-       char* filename = "number.txt";
-       fp = fopen(filename, "r");
-       if (fp == NULL){
-           printf("Could not open file %s",filename);
-           return 1;
-       }
-    int i = 0;
-    while (fgets(str, MAXCHAR, fp) != NULL){
-        int num = atoi(str);
-        arr[i] = num;
-        i += 1;
-    }
-    fclose(fp);
+
     
     int sum0,sum1,sum2,sum3;
     if(rank == 0){
-        sum0 = 0;
-        for(i = 0; i < 16; i++){
-            sum0 += array[i];
+        int *arr = (int*)(malloc(sizeof(int)*64));
+        FILE *fp;
+           char str[MAXCHAR];
+           char* filename = "number.txt";
+           fp = fopen(filename, "r");
+           if (fp == NULL){
+               printf("Could not open file %s",filename);
+               return 1;
+           }
+        int i = 0;
+        while (fgets(str, MAXCHAR, fp) != NULL){
+            int num = atoi(str);
+            arr[i] = num;
+            i += 1;
         }
-        MPI_Recv(&sum1, 1, MPI_INT, 1, 200, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
-        MPI_Recv(&sum2, 1, MPI_INT, 2, 200, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
-        MPI_Recv(&sum3, 1, MPI_INT, 3, 200, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
-        
-        sum0 += sum1 + sum2 + sum3;
-        printf("final Sum: %d",sum0);
+        fclose(fp);
+        MPI_Bcast(&, 10, MPI_CHAR, 0, MPI_COMM_WORLD);
+
     }
     else if(rank == 1){
         int* array = arr;
         sum1 = 0;
         int i;
-        for(i = 16; i < 32; i++){
+        for(i = 0; i < 16; i++){
             sum1 += array[i];
         }
         MPI_Send(&sum1, 1, MPI_INT, 0, 200, MPI_COMM_WORLD);
@@ -52,8 +46,8 @@ int main(int argc, char** argv)
     else if(rank == 2){
         int* array = arr;
         sum2 = 0;
-        int j;
-        for(j = 32; j < 48; i++){
+        int i;
+        for(i = 32; i < 48; i++){
             sum2 += array[i];
         }
         MPI_Send(&sum2, 1, MPI_INT, 0, 200, MPI_COMM_WORLD);
@@ -61,8 +55,8 @@ int main(int argc, char** argv)
     else if(rank == 3){
       int* array = arr;
       sum3 = 0;
-      int k;
-      for(k = 48; k < 64; i++){
+      int i;
+      for(i = 48; i < 64; i++){
           sum3 += array[i];
       }
       MPI_Send(&sum3, 1, MPI_INT, 0, 200, MPI_COMM_WORLD);
